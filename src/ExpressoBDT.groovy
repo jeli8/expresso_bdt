164d50e6ffdddcf7e4b585c50eb08b119869fcb5
Paul-Anthony Dudzinski <paul.dudzinski@nielsen.com>
2017-01-26 17:05:25 -0500

    Committing source code for first groovy + spock test. Queries expresso for json object and checks to see if it returns a map when parsed.

---
+++ src/ExpressoBDT.groovy
@@ -0,0 +1,25 @@
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import groovyx.net.http.RESTClient
+import spock.lang.*
+
+// Hit 'Run Script' below
+class Expresso extends Specification {
+    def "testing for valid output to debug"() {
+        def client = new RESTClient( 'http://loadus.exelator.com/load/' )
+        def requestParams = [ p : '227', g : '001', dbg : 'smtdn9', response : 'json' ]
+        def resp = client.get(path : '/load', query : requestParams)
+        def jsonSlurper = new JsonSlurper()
+
+        expect:
+            resp.status == 200  // HTTP response code is 200
+            def object = resp.getData() // Get data and set to object
+            def map = jsonSlurper.parseText(object.toString()) // Json Debug
+            map instanceof Map
+
+        cleanup:
+            println JsonOutput.prettyPrint(object.toString())
+    }
+}
+
+
