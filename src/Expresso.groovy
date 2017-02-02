450c806ffa9e8f0a25a7eabe0fc40d9dc246508f
Paul-Anthony Dudzinski <paul.dudzinski@nielsen.com>
2017-02-02 14:41:44 -0500

    Adding some additional tests

--- src/Expresso.groovy
+++ src/Expresso.groovy
@@ -5,20 +5,36 @@ import spock.lang.*
 
 
 class ExpressoOutput extends Specification {
-    def "testing for valid output to debug"() {
-        def client = new RESTClient( 'http://loadus.exelator.com/load/' )
-        def requestParams = [ p : '227', g : '001', dbg : 'smtdn9', response : 'json' ]
-        def resp = client.get(path : '/load', query : requestParams)
-        def jsonSlurper = new JsonSlurper()
+        @Shared exp_uri = 'http://nyjexp001.exelator.com:8080'
+        @Shared client = new RESTClient()
+        @Shared requestParams = [ p : '227', g : '001', dbg : 'smtdn9', response : 'json' ]
+        @Shared objResp = client.get(path : '/load', query : requestParams, uri: exp_uri)
+    def "response is 200"() {
+        expect:
+            objResp.status == 200  // HTTP response code is 200
+    }
+    def "testing for valid json output to debug"() {
+        setup:
+            def object = objResp.getData()
+            def jsonSlurper = new JsonSlurper()
+            def resp_map = jsonSlurper.parseText(object.toString()) // Json Debug
 
         expect:
-            resp.status == 200  // HTTP response code is 200
-            def object = resp.getData() // Get data and set to object
-            def map = jsonSlurper.parseText(object.toString()) // Json Debug
-            map instanceof Map
+            resp_map instanceof Map
 
         cleanup:
             println JsonOutput.prettyPrint(object.toString())
+    }
+    def "test for all required parts of the debug object"() {
+        setup:
+            def object = objResp.getData()
+            def jsonSlurper = new JsonSlurper()
+            def resp_map = jsonSlurper.parseText(object.toString()) // Json Debug
+
+        expect:
+            resp_map.containsKey("flowStatus")
+            resp_map.containsKey("logsMap")
+            resp_map.containsKey("kafkaMap")
     }
 }
 
