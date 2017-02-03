1992d631b4176376ba3d18e33778c7648146b09d
Paul-Anthony Dudzinski <paul.dudzinski@nielsen.com>
2017-02-03 14:40:53 -0500

    Turned the expresso call and restclient into a class

--- expressoOutput/src/Expresso.groovy
+++ expressoOutput/src/Expresso.groovy
@@ -2,39 +2,66 @@ import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 import groovyx.net.http.RESTClient
 import spock.lang.*
+import spock.util.mop.Use
+
+class ExpressoCall {
+    /*TODO Implement POST for batch calls */
+    private String uri
+    private String path
+    private Map<String,?> requestParams
+    private client = new RESTClient()
+    private Object restResponse
+    private int responseCode
+    private Map<String, ?> response
+
+    ExpressoCall(String uri, Map requestParams, String path) {
+        this.uri = uri
+        this.requestParams = requestParams
+        this.path = path
+    }
+
+    def getCall(){
+        this.restResponse = this.client.get(path : this.path, query : this.requestParams, uri: this.uri)
+        this.responseCode = this.restResponse.status
+        this.getJsonMap()
+    }
+
+    def getJsonMap(){
+        def call = this.restResponse.getData()
+        def jsonSlurper = new JsonSlurper()
+        this.response = jsonSlurper.parseText(call.toString())
+    }
+
+}
 
 
 class ExpressoOutput extends Specification {
-    @Shared exp_uri = 'http://nyjexp001.exelator.com:8080'
-    @Shared client = new RESTClient()
-    @Shared requestParams = [p: '227', g: '001', dbg: 'smtdn9', response: 'json' ]
-    @Shared objResp = client.get(path : '/load', query : requestParams, uri: exp_uri)
+    @Shared expCall = new ExpressoCall(
+            'http://nyjexp010.exelator.com:8080',
+            [p: '227', g: '001', dbg: 'smtdn9', response: 'json' ],
+            '/load')
+
+    def expressoHelper(expCall){
+        expCall.getCall()
+    }
+
     def "response is 200"() {
+        setup:
+            expressoHelper(expCall)
         expect:
-            objResp.status == 200  // HTTP response code is 200
+            expCall.responseCode == 200  // HTTP response code is 200
     }
     def "testing for valid json output to debug"() {
-        setup:
-            def object = objResp.getData()
-            def jsonSlurper = new JsonSlurper()
-            def resp_map = jsonSlurper.parseText(object.toString()) // Json Debug
-
         expect:
-            resp_map instanceof Map
+            expCall.response instanceof Map
 
-        cleanup:
-            println JsonOutput.prettyPrint(object.toString())
     }
     def "test for all required parts of the debug object"() {
-        setup:
-            def object = objResp.getData()
-            def jsonSlurper = new JsonSlurper()
-            def resp_map = jsonSlurper.parseText(object.toString()) // Json Debug
-
         expect:
-            resp_map.containsKey("flowStatus")
-            resp_map.containsKey("logsMap")
-            resp_map.containsKey("kafkaMap")
+            expCall.response.containsKey("flowStatus")
+            expCall.response.containsKey("logsMap")
+            expCall.response.containsKey("kafkaMap")
     }
+
 }
 
